e814a4c87177ed61963a722c8209d3931db977ae
TakaGui <gui.yukio175@gmail.com>
2020-09-24 17:13:11 -0300

    Add and remove indicator current slide

--- front-end/web-accessibility/apeperia/js/carousel.js
+++ front-end/web-accessibility/apeperia/js/carousel.js
@@ -6,10 +6,15 @@ var new2 = document.querySelector('#new2');
 
 new0.style.display = 'block';
 
+// Criando indicador de slide atual
+var indicadorSlideAtual = document.createElement('span');
+indicadorSlideAtual.classList.add('escondeVisualmente');
+indicadorSlideAtual.id = 'escondeVisualmente';
+indicadorSlideAtual.textContent = '(Slide atual)';
+
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
-    
     if (this.getAttribute('data-sliderItem') === '0') {
       new0.style.display = 'block';
       new1.style.display = 'none';
@@ -23,6 +28,10 @@ btns.forEach(function(btn) {
       new1.style.display = 'none';
       new2.style.display = 'block';
     }
+    
+    document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
+
+    this.append(indicadorSlideAtual);
 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
