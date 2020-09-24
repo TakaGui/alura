38157ca57f89f777855b85b1125789a81acd8dbe
TakaGui <gui.yukio175@gmail.com>
2020-09-23 22:18:15 -0300

    Add inert and focus dialog

--- front-end/web-accessibility/apeperia/js/dialog.js
+++ front-end/web-accessibility/apeperia/js/dialog.js
@@ -3,6 +3,7 @@ var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
 
 btnAbreDialog.style.display = 'block';
 
@@ -10,11 +11,14 @@ btnAbreDialog.style.display = 'block';
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
   document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus();
 }
 
 // Listeners
