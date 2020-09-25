4cabdc6f4f73992c6b03f02e45da8426956fb86a
TakaGui <gui.yukio175@gmail.com>
2020-09-25 00:01:18 -0300

    Check if email is incorrect

---
+++ front-end/web-accessibility/apeperia/js/mailcheck-config.js
@@ -0,0 +1,18 @@
+var campoEmail = document.querySelector('#email');
+var sugestao = document.querySelector('#sugestao');
+
+var domains = ['gmail.com', 'uol.com', 'outlook.com'];
+var secondLevelDomains = ['hotmail']
+var topLevelDomains = ["com", "net", "org", 'br'];
+
+campoEmail.addEventListener('blur', function() {
+  Mailcheck.run({
+    email: campoEmail.value,
+    domains: domains,                       // optional
+    topLevelDomains: topLevelDomains,       // optional
+    secondLevelDomains: secondLevelDomains, // optional
+    suggested: function(suggestion) {
+      // callback code
+    }
+  });
+});
