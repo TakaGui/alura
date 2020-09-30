3ee56ef2947609f3eb37b74fd38dc158e8c3e9b0
TakaGui <gui.yukio175@gmail.com>
2020-09-30 08:21:48 -0300

    Add patient form and function add to table

--- front-end/javascript-introduction/js/principal.js
+++ front-end/javascript-introduction/js/principal.js
@@ -1,8 +1,7 @@
 var titulo = document.querySelector('.titulo');
+var pacientes = document.querySelectorAll('.paciente');
 
 titulo.textContent = "Aparecida Nutricionista";
-
-var pacientes = document.querySelectorAll('.paciente');
 
 pacientes.forEach(paciente => {
   var tdPeso = paciente.querySelector('.info-peso');
@@ -33,4 +32,38 @@ pacientes.forEach(paciente => {
   
     tdImc.textContent = imc.toFixed(2);
   }
+});
+
+var botaoAdicionarPaciente = document.querySelector('#adicionar-paciente');
+botaoAdicionarPaciente.addEventListener('click', function (event) {
+  event.preventDefault();
+  
+  var form = document.querySelector('#form-adiciona');
+
+  var nome = form.nome.value;
+  var peso = form.nome.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement('tr');
+
+  var nomeTd = document.createElement('td');
+  var pesoTd = document.createElement('td');
+  var alturaTd = document.createElement('td');
+  var gorduraTd = document.createElement('td');
+  var imcTd = document.createElement('td');
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector('#tabela-pacientes');
+
+  tabela.appendChild(pacienteTr);
 });
