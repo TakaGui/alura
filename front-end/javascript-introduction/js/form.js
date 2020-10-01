5cce284c3f5bd52f92626cce4bdd4b0b26a907af
TakaGui <gui.yukio175@gmail.com>
2020-10-01 08:13:40 -0300

    Refactor files js

--- front-end/javascript-introduction/js/form.js
+++ front-end/javascript-introduction/js/form.js
@@ -1,40 +1,5 @@
-var titulo = document.querySelector('.titulo');
-var pacientes = document.querySelectorAll('.paciente');
-
-titulo.textContent = "Aparecida Nutricionista";
-
-pacientes.forEach(paciente => {
-  var tdPeso = paciente.querySelector('.info-peso');
-  var peso = tdPeso.textContent;
-  
-  var tdAltura = paciente.querySelector('.info-altura');
-  var altura = tdAltura.textContent;
-  
-  var tdImc = paciente.querySelector('.info-imc');
-  
-  var pesoValido = true;
-  var alturaValida = true;
-  
-  if ((peso <= 0) || (peso >= 1000)) {
-    pesoValido = false;
-    tdImc.textContent = 'Peso inválido';
-    paciente.classList.add('paciente-invalido');
-  }
-  
-  if ((altura <= 0) || (altura >= 3.00)) {
-    alturaValida = false;
-    tdImc.textContent = 'Altura inválida';
-    paciente.classList.add('paciente-invalido');
-  }
-  
-  if (pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-  
-    tdImc.textContent = imc.toFixed(2);
-  }
-});
-
 var botaoAdicionarPaciente = document.querySelector('#adicionar-paciente');
+
 botaoAdicionarPaciente.addEventListener('click', function (event) {
   event.preventDefault();
   
