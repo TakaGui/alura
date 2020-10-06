98d71d2932b027d3e89897a59d814a5094fa37d4
Taka <gui.yukio175@gmail.com>
2020-10-05 22:18:28 -0300

    Catch error request

--- front-end/javascript-introduction/js/importar-pacientes.js
+++ front-end/javascript-introduction/js/importar-pacientes.js
@@ -6,13 +6,24 @@ importarPaciente.addEventListener('click', function() {
   xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
 
   xhr.addEventListener('load', function() {
-    var resposta = xhr.responseText;
+    var erroAjax = document.querySelector('#erro-ajax');
 
-    var pacientes = JSON.parse(resposta);
-
-    pacientes.forEach(paciente => {
-      adicionaPacienteNaTabela(paciente);
-    });
+    if (xhr.status == 200) {
+      erroAjax.classList.add('invisivel');
+      
+      var resposta = xhr.responseText;
+  
+      var pacientes = JSON.parse(resposta);
+  
+      pacientes.forEach(paciente => {
+        adicionaPacienteNaTabela(paciente);
+      });
+    } else {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+    
+      erroAjax.classList.remove('invisivel');
+    }
   });
 
   xhr.send();
