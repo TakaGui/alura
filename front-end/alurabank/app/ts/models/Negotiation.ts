502d90ec4438032ed5fd3305724e984dc654922b
takagui <gui.yukio175@gmail.com>
2021-03-03 14:47:38 -0300

    feat: type model negotiation

--- front-end/alurabank/app/ts/models/Negotiation.ts
+++ front-end/alurabank/app/ts/models/Negotiation.ts
@@ -1,13 +1,9 @@
 class Negotiation {
-  private _date;
-  private _quantity;
-  private _value;
-
-  constructor(date, quantity, value) {
-    this._date = date;
-    this._quantity = quantity;
-    this._value = value;
-  }
+  constructor(
+    private _date: Date,
+    private _quantity: number,
+    private _value: number
+  ) {}
 
   get date() {
     return this._date;
