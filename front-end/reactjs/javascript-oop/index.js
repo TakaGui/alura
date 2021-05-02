98ff301d63179d37ef6a3774de295bd6fba7efce
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-02 20:10:17 -0300

    feat: create method deposit and change attribute balance to private

--- front-end/reactjs/javascript-oop/index.js
+++ front-end/reactjs/javascript-oop/index.js
@@ -5,12 +5,22 @@ class Cliente {
 
 class ContaCorrente {
   agencia;
-  saldo;
+  #saldo = 0;
+
+  depositar(valor) {
+    if (valor > 0) {
+      this.#saldo += valor;
+    }
+  }
 
   sacar(valor) {
-    if (this.saldo >= valor) {
-      this.saldo -= valor;
+    if (this.#saldo >= valor) {
+      this.#saldo -= valor;
     }
+  }
+
+  mostrarSaldo() {
+    return this.#saldo;
   }
 }
 
@@ -27,16 +37,14 @@ cliente2.cpf = 44455566609;
 const contaCorrenteTaka = new ContaCorrente();
 
 contaCorrenteTaka.agencia = 1001;
-contaCorrenteTaka.saldo = 0;
-console.log(contaCorrenteTaka.saldo);
+console.log(contaCorrenteTaka.mostrarSaldo());
 
-// depósito
-contaCorrenteTaka.saldo = 100;
-console.log(contaCorrenteTaka.saldo);
+contaCorrenteTaka.depositar(500);
+console.log(contaCorrenteTaka.mostrarSaldo());
 
 contaCorrenteTaka.sacar(200);
 
-console.log(contaCorrenteTaka.saldo);
+console.log(contaCorrenteTaka.mostrarSaldo());
 
 console.log(cliente1);
 console.log(cliente2);
