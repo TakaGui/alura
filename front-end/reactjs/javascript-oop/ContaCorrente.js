e3c7f1241b260d5b191065bfbbbea78e999abb02
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-05 11:12:44 -0300

    feat: create method transfer

--- front-end/reactjs/javascript-oop/ContaCorrente.js
+++ front-end/reactjs/javascript-oop/ContaCorrente.js
@@ -1,5 +1,7 @@
 export class ContaCorrente {
+  cliente;
   agencia;
+
   #saldo = 0;
 
   depositar(valor) {
@@ -24,5 +26,10 @@ export class ContaCorrente {
 
   mostrarSaldo() {
     return this.#saldo;
+  }
+
+  transferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
   }
 }
--- front-end/reactjs/javascript-oop/index.js
+++ front-end/reactjs/javascript-oop/index.js
@@ -13,14 +13,29 @@ cliente2.cpf = 44455566609;
 
 const contaCorrenteTaka = new ContaCorrente();
 
+contaCorrenteTaka.cliente = cliente1;
 contaCorrenteTaka.agencia = 1001;
-console.log('saldo', contaCorrenteTaka.mostrarSaldo());
+
+console.log(`saldo ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka.mostrarSaldo());
 
 const valorDepositado = contaCorrenteTaka.depositar(500);
-console.log('valor depositado', valorDepositado);
-console.log('saldo', contaCorrenteTaka.mostrarSaldo());
+console.log(`valor depositado ${contaCorrenteTaka.cliente.nome}`, valorDepositado);
+console.log(`saldo ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka.mostrarSaldo());
 
 const valorSacado = contaCorrenteTaka.sacar(200);
-console.log('valor sacado', valorSacado);
+console.log(`valor sacado ${contaCorrenteTaka.cliente.nome}`, valorSacado);
 
-console.log('saldo', contaCorrenteTaka.mostrarSaldo());
+console.log(`saldo ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka.mostrarSaldo());
+
+console.log(`conta corrente ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka);
+
+const contaCorrenteKaren = new ContaCorrente();
+
+contaCorrenteKaren.cliente = cliente2;
+contaCorrenteKaren.agencia = 102;
+
+contaCorrenteTaka.transferir(100, contaCorrenteKaren);
+
+console.log('conta corrente Karen', contaCorrenteKaren);
+console.log(`saldo ${contaCorrenteKaren.cliente.nome}`, contaCorrenteKaren.mostrarSaldo());
+console.log(`saldo ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka.mostrarSaldo());
