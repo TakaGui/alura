05c722f051a65decb7e0c3236804de5f9fc06b5e
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-05 13:02:07 -0300

    refactor: create client

--- front-end/reactjs/javascript-oop/index.js
+++ front-end/reactjs/javascript-oop/index.js
@@ -1,21 +1,25 @@
 import { Cliente } from './Cliente.js';
 import { ContaCorrente } from './ContaCorrente.js';
 
-const cliente1 = new Cliente();
-
-cliente1.nome = 'Taka';
-cliente1.cpf = 11122233309;
-
-const cliente2 = new Cliente();
-
-cliente2.nome = 'Karen';
-cliente2.cpf = 44455566609;
-
+// Taka
 const contaCorrenteTaka = new ContaCorrente();
 
-contaCorrenteTaka.cliente = cliente1;
+contaCorrenteTaka.cliente = new Cliente();
 contaCorrenteTaka.agencia = 1001;
 
+contaCorrenteTaka.cliente.nome = "Taka"
+contaCorrenteTaka.cliente.cpf = 11122233309;
+
+//Karen
+const contaCorrenteKaren = new ContaCorrente();
+
+contaCorrenteKaren.cliente = new Cliente();
+
+contaCorrenteKaren.cliente.nome = "Karen"
+contaCorrenteKaren.cliente.cpf = 44455566609;
+contaCorrenteKaren.agencia = 102;
+
+// Movimentações
 console.log(`saldo ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka.mostrarSaldo());
 
 const valorDepositado = contaCorrenteTaka.depositar(500);
@@ -28,11 +32,6 @@ console.log(`valor sacado ${contaCorrenteTaka.cliente.nome}`, valorSacado);
 console.log(`saldo ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka.mostrarSaldo());
 
 console.log(`conta corrente ${contaCorrenteTaka.cliente.nome}`, contaCorrenteTaka);
-
-const contaCorrenteKaren = new ContaCorrente();
-
-contaCorrenteKaren.cliente = cliente2;
-contaCorrenteKaren.agencia = 102;
 
 contaCorrenteTaka.transferir(100, contaCorrenteKaren);
 
