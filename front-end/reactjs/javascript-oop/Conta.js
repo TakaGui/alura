09ebe1a4522fcd43ddf120f397f14f5a7e58c96c
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-06 10:30:39 -0300

    feat: override method withdraw

--- front-end/reactjs/javascript-oop/Conta.js
+++ front-end/reactjs/javascript-oop/Conta.js
@@ -1,10 +1,11 @@
+import { Cliente } from './Cliente.js';
 export class Conta {
-  #saldo;
+  _saldo;
   #agencia;
   #cliente;
 
   constructor(saldoInicial, agencia, cliente) {
-    this.#saldo = saldoInicial;
+    this._saldo = saldoInicial;
     this.#agencia = agencia;
     this.#cliente = cliente;
   }
@@ -20,7 +21,7 @@ export class Conta {
   }
 
   get saldo() {
-    return this.#saldo;
+    return this._saldo;
   }
 
   get agencia() {
@@ -29,22 +30,30 @@ export class Conta {
 
   depositar(valor) {
     if (valor <= 0) {
-      return;
+      return 0;
     }
 
-    this.#saldo += valor;
+    this._saldo += valor;
 
     return valor;
   }
 
   sacar(valor) {
-    if (this.#saldo <= valor) {
-      return
+    let taxa = 1;
+
+    return this._sacar(valor, taxa);
+  }
+
+  _sacar(valor, taxa) {
+    const valorSacado = taxa * valor;
+
+    if (this._saldo <= valorSacado) {
+      return 0;
     }
 
-    this.#saldo -= valor;
+    this._saldo -= valorSacado;
 
-    return valor;
+    return valorSacado;
   }
 
   transferir(valor, conta) {
--- front-end/reactjs/javascript-oop/ContaCorrente.js
+++ front-end/reactjs/javascript-oop/ContaCorrente.js
@@ -1,4 +1,3 @@
-import { Cliente } from './Cliente.js';
 import { Conta } from './Conta.js';
 
 export class ContaCorrente extends Conta {
@@ -8,5 +7,12 @@ export class ContaCorrente extends Conta {
     super(0, agencia, cliente);
 
     ContaCorrente.numeroDeContas += 1;
+  }
+
+  // sobrescrevendo o comportamento de sacar
+  sacar(valor) {
+    let taxa = 1.1;
+
+    return this._sacar(valor, taxa);
   }
 }
