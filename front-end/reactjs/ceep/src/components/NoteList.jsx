999c2444b6d00645ae3f40735bee8f5aeaade834
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-11 20:03:09 -0300

    feat: integrate js

--- front-end/reactjs/ceep/src/components/NoteList.jsx
+++ front-end/reactjs/ceep/src/components/NoteList.jsx
@@ -6,9 +6,17 @@ export class NoteList extends Component {
   render() {
     return (
       <ul>
-        <li>
-          <NoteCard />
-        </li>
+        {
+          Array.of('Trabalho', 'Trabalho', 'Estudo')
+            .map((category, index) => {
+              return (
+                <li key={index}>
+                  <div>{category}</div>
+                  <NoteCard />
+                </li>
+              );
+            })
+        }
       </ul>
     );
   }
