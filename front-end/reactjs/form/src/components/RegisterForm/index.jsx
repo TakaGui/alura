d88c0ce774735bc890731bed9dc368b7d8d3b264
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-15 18:45:08 -0300

    feat: conditional render

--- front-end/reactjs/form/src/components/RegisterForm/index.jsx
+++ front-end/reactjs/form/src/components/RegisterForm/index.jsx
@@ -1,19 +1,45 @@
-import { PersonalData } from './PersonalData';
+import { useState } from 'react';
+import { Typography } from '@material-ui/core';
+
 import { UserData } from './UserData';
+import { PersonalData } from './PersonalData';
 import { AddressData } from './AddressData';
 
 export function RegisterForm({
   onSubmitForm,
   validCpf,
 }) {
+  const [stage, setStage] = useState(1);
+
+  const currentForm = (stage) => {
+    switch (stage) {
+      case 0:
+        return (<UserData />);
+      case 1:
+        return (
+          <PersonalData
+            onSubmitForm={onSubmitForm}
+            validCpf={validCpf}
+          />
+        );
+      case 2:
+        return (<AddressData />);
+      default:
+        return (
+          <Typography
+            variant="h4"
+            component="h2"
+            align="center"
+          >
+            Erro ao selecionar formulário
+          </Typography>
+        );
+    }
+  }
+
   return (
     <>
-      <PersonalData
-        onSubmitForm={onSubmitForm}
-        validCpf={validCpf}
-      />
-      <UserData />
-      <AddressData />
+      {currentForm(stage)}
     </>
   );
 }
