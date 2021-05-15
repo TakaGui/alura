50be873cb6a740b3de731f27e578b3454676493c
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-14 21:51:29 -0300

    feat: create form

---
+++ front-end/reactjs/form/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { RegisterForm } from './components/RegisterForm';
+
+function App() {
+  return (
+    <>
+      <h1>Formulário de cadastro</h1>
+      <RegisterForm />
+    </>
+  );
+}
+
+export default App;
---
+++ front-end/reactjs/form/src/components/RegisterForm/index.jsx
@@ -0,0 +1,30 @@
+import Button from '@material-ui/core/Button';
+
+export function RegisterForm() {
+  return (
+    <form action="">
+      <label htmlFor="">Nome</label>
+      <input type="text" />
+
+      <label htmlFor="">Sobrenome</label>
+      <input type="text" />
+
+      <label htmlFor="">CPF</label>
+      <input type="text" />
+
+      <label htmlFor="">Promoções</label>
+      <input type="checkbox" />
+
+      <label htmlFor="">Novidades</label>
+      <input type="checkbox" />
+
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+      >
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
