949d4e8b6030b786c1948973bdc989ea8c3d83b0
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-16 10:47:26 -0300

    refactor: show forms

--- front-end/reactjs/form/src/components/RegisterForm/index.jsx
+++ front-end/reactjs/form/src/components/RegisterForm/index.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { Typography } from '@material-ui/core';
 
 import { UserData } from './UserData';
 import { PersonalData } from './PersonalData';
@@ -15,43 +14,22 @@ export function RegisterForm({
     setStage(stage + 1);
   }
 
-  const currentForm = (stage) => {
-    switch (stage) {
-      case 0:
-        return (
-          <UserData
-            onSubmitForm={nextStage}
-          />
-        );
-      case 1:
-        return (
-          <PersonalData
-            onSubmitForm={nextStage}
-            validCpf={validCpf}
-          />
-        );
-      case 2:
-        return (
-          <AddressData
-            onSubmitForm={onSubmitForm}
-          />
-        );
-      default:
-        return (
-          <Typography
-            variant="h4"
-            component="h2"
-            align="center"
-          >
-            Erro ao selecionar formulário
-          </Typography>
-        );
-    }
-  }
+  const forms = [
+    <UserData
+      onSubmitForm={nextStage}
+    />,
+    <PersonalData
+      onSubmitForm={nextStage}
+      validCpf={validCpf}
+    />,
+    <AddressData
+      onSubmitForm={onSubmitForm}
+    />,
+  ];
 
   return (
     <>
-      {currentForm(stage)}
+      {forms[stage]}
     </>
   );
 }
