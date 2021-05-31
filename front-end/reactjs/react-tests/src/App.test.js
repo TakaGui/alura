2ffc82d4761b3e53ebfb5f6f4d38ba179830810c
Guilherme Takahashi <taka.gui@outlook.com>
2021-05-31 14:11:53 -0300

    test: transaction

--- front-end/reactjs/react-tests/src/App.test.js
+++ front-end/reactjs/react-tests/src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import App from './App';
+import App, {
+  calcularNovoSaldo,
+} from './App';
 
 describe('Componente principal', () => {
   describe('Quando eu abro o app do banco', () => {
@@ -21,6 +23,34 @@ describe('Componente principal', () => {
       render(<App />);
 
       expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+    });
+  });
+
+  describe('Quando eu realizo uma transação', () => {
+    it('que é um saque, o valor deve diminuir', () => {
+      const valores = {
+        transacao: 'saque',
+        valor: 50,
+      };
+
+      const saldo = 150;
+
+      const novoSaldo = calcularNovoSaldo(valores, saldo);
+
+      expect(novoSaldo).toBe(100);
+    });
+
+    it('que é um depósito, o valor deve diminuir', () => {
+      const valores = {
+        transacao: 'deposito',
+        valor: 50,
+      };
+
+      const saldo = 150;
+
+      const novoSaldo = calcularNovoSaldo(valores, saldo);
+
+      expect(novoSaldo).toBe(200);
     });
   });
 });
